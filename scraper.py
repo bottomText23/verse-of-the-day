ecf2ce44390de2b1185b1fcfa544c91876536492
Francisco <franciscocastro2022@hotmail.com>
2025-09-16 20:38:20 +0200

    worked on scraping code

--- scraper.py
+++ scraper.py
@@ -1,2 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+
+#get the html of the website
+webpage = requests.get("https://www.verseoftheday.com/en/")
+soup = BeautifulSoup(webpage.text, "html.parser")
+
+#get quote and verse in array n_text
+s_text = soup.find_all("div", attrs={"class":"bilingual-left"})
+n_text = s_text[0].text.split("—")
+
+quote = n_text[0]
+verse = n_text[1]
+
+print(quote)
+print(verse)

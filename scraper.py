98aebc88c801f04f2015315b5a88ed9a402a7284
Francisco <franciscocastro2022@hotmail.com>
2025-09-22 12:46:23 +0200

    fixed issue with '—' character

--- scraper.py
+++ scraper.py
@@ -11,11 +11,12 @@ def scrape():
     webpage = requests.get("https://www.verseoftheday.com/en/")
     soup = BeautifulSoup(webpage.text, "html.parser")
 
-    #get quote and verse in array n_text
-    s_text = soup.find_all("div", attrs={"class":"bilingual-left"})
-    n_text = s_text[0].text.split("—")
-
-    quote = n_text[0]
-    verse = n_text[1]
+    #get quote and verse
+    s_quote = soup.find("div", attrs={"class":"bilingual-left"})
+    s_verse = soup.find("div", attrs={"class":"reference"})
+    
+    #get only verse text for quote
+    quote = s_quote.contents[0].text
+    verse = s_verse.text
 
     return split_text(quote, verse)
